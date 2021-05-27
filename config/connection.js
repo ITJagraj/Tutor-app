19b42f3d24f58d4501eebb6b74a5bcbf9f82ea4c
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-05-27 16:40:30 -0400

    did a bunch of work on server.js and connections.js

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+// import sequelize 
+const { Sequelize } = require("sequelize");
+const sequelize = require("sequelize");
+
+require ("dotenv").config(); //for loading env variables 
+
+//create connection for the databadse
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    });
