f0fdab5dd7613a75bf1d7360397d0c702b847ad4
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-05-27 21:56:29 -0400

    created apis

---
+++ routes/api/User.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const withAuth = require('../../auth');
+const { User } = require('../../models');
+
+//GET /api/users/1
+router.get('/:id', withAuth, (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: {
+            exclude: ['password'],
+
+        }
+})
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+// DELETE /api/users/1
+router.delete('/:id', withAuth, (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
