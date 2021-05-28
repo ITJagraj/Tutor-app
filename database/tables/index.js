ce8ab5233a9e038a6448c46fb772b664e4772146
Your Name <you@example.com>
2021-05-27 23:35:44 -0400

    connection tables

--- database/tables/index.js
+++ database/tables/index.js
@@ -3,6 +3,7 @@ const Category = require('./Category');
 const Question = require('./Question');
 const Answer = require('./Answer');
 const Organization = require('./Organization');
+const CategoryQuestion = require('./CategoryQuestion')
 
 User.hasMany(Question, {
     foreignKey: 'user_id'
@@ -13,13 +14,33 @@ Question.belongsTo(User, {
 });
 
 Question.belongsToMany(Category, {
+    through: CategoryQuestion,
+    as: 'category_question',
     foreignKey: 'question_id'
 });
 
 Category.belongsToMany(Question, {
+    through: CategoryQuestion,
+    as: 'category_question',
     foreignKey: 'category_id'
+});
+
+User.hasMany(Answer, {
+    foreignKey: 'user_id'
+});
+
+Answer.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Question.hasMany(Answer, {
+    foreignKey: 'question_id'
+});
+
+Answer.belongsTo(Question, {
+    foreignKey: 'question_id'
 });
 
 
 
-module.exports = { User, Category, Question, Answer, Organization };
+module.exports = { User, Category, Question, Answer, Organization, CategoryQuestion };
