ff61f610a37b0e2f819c79ef32252bf4261736ad
Andre M <andre.ji.moseley@gmail.com>
2021-05-27 23:23:13 -0400

    added many to many relationship table

---
+++ database/tables/CategoryQuestion.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class CategoryQuestion extends Model {};
+
+    // define table columns and configuration
+CategoryQuestion.init(
+    {
+      // define an id column
+      id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        // instruct that this is the Primary Key
+        primaryKey: true,
+        // turn on auto increment
+        autoIncrement: true
+      },
+      category_id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        references: {
+            model: 'category',
+            key: 'id'
+        }
+      },
+      question_id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        references: {
+            model: 'question',
+            key: 'id'
+        }
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'category_question'
+    }
+  );
+
+
+module.exports = CategoryQuestion;
