75994b73bae77709ad27e7a452c41ab0d5cab9b7
Andre M <andre.ji.moseley@gmail.com>
2021-05-28 20:51:55 -0400

    fixed the server.js paths & exports

--- config/connection.js
+++ config/connection.js
@@ -7,12 +7,13 @@ require ("dotenv").config(); //for loading env variables
 //create connection for the databadse
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: "localhost",
         dialect: "mysql",
         dialectOptions: {
             decimalNumbers: true,
         },
+        port: 3306
     });
 
 module.exports = sequelize;
