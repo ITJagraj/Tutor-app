936b8b2d0b16d6453d7ec2d65c67fb5aeb9c95af
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-06-01 18:03:42 -0400

    troubleshooting the logout problem and post page

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -2,12 +2,12 @@ async function searchFormHandler(event) {
     event.preventDefault();
 
     const searchText= document.querySelector('#search-text').value.toUpperCase();
-
+console.log("test")
     const miniSearch= new miniSearch({
         fields: ['question_title', 'question_text'],
         storedFields: ['question_title', 'question_text']
     })
-
+console.log(miniSearch) //this is for test purposes
     miniSearch.addAll(questiondata)
 
     let results= miniSearch.search(searchText)
@@ -20,13 +20,13 @@ async function searchFormHandler(event) {
     
     if (response.ok) {
         // ???? targeted location api
-        document.location.replace('/search')
+        document.location.replace('/search.handlebars')
     }
     else {
-        // ???????????????????????/
+        console.log("pathway not working")
+        alert(response.statusText);
     }
 };
-
 
 
 document.querySelector('#search-id').addEventListener('click', searchFormHandler);
