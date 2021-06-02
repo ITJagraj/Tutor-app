1e8f11b13b3f6a3b82de2e4c6204cc936cdaa2b3
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-06-01 22:42:00 -0400

    making lots of process

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -65,7 +65,9 @@ router.post('/', (req, res) => {
             req.session.save(() => {
                 req.session.user_id = dbUserData.id;
                 req.session.username = dbUserData.username;
-                req.session.loggedIn = false;
+                req.session.loggedIn = true;
+                console.log(req.session.username)
+                console.log("test", req.session.loggedIn)
 
                 res.json(dbUserData);
             });
@@ -97,7 +99,8 @@ router.post('/login', (req, res) => {
                 req.session.user_id = dbUserData.id;
                 req.session.username = dbUserData.username;
                 req.session.loggedIn = true;
-
+                console.log(req.session.username)
+                console.log("test", req.session.loggedIn)
                 res.json(dbUserData);
 
                 res.json({user: dbUserData, message: 'You are now logged in!'});
